49e1c19a5fd61f5695d86f4c207046d9cf9f5ac7
Sirius Black <mariia.beliaev@gmail.com>
2023-04-19 18:23:35 +0300

    added binarySearch function and tests

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 // Этот метод должен искать элемент в массиве и возвращать его индекс или -1, если элемент не находится в массиве.
 // Например, если ты ищешь G в массиве [A, B, C, D, E, F, G, H], твой метод должен возвращать 6. Если ты ищешь X, он должен возвращать -1.
 
-function binarySearch (){
-
+function binarySearch(sortedArr, item) {
+  let start = 0;
+  let end = sortedArr.length - 1;
+  while (start < end) {
+    let middle = Math.floor((start + end) / 2);
+    if (sortedArr[middle] === item) {
+      return middle;
+    } else if (sortedArr[middle] < item) {
+      start = middle + 1;
+    } else {
+      end = middle - 1;
+    }
+  }
+  return -1;
 }
 
-module.exports = binarySearch
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 7));
+module.exports = binarySearch;
--- spec/binarysearch.spec.js
+++ spec/binarysearch.spec.js
@@ -1,13 +1,17 @@
-const binarySearch = require('../index')
+const binarySearch = require("../index");
 
-test('Функция должна возвращать индекс элемента', () => {
-    
-})
+test("Функция должна возвращать индекс элемента", () => {
+  let item = 7;
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  expect(binarySearch(arr, item)).toEqual(6);
+});
 
-test('Функция должна быть определена', () => {
-    
-})
+test("Функция должна быть определена", () => {
+  expect(binarySearch).toBeDefined();
+});
 
-test('Функция должна возвращать -1, если элемента не существует в данном массиве', () => {
-    
-})
+test("Функция должна возвращать -1, если элемента не существует в данном массиве", () => {
+  let item = 10;
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  expect(binarySearch(arr, item)).toEqual(-1);
+});
